2a24dfb358387993cfdb709200993475473e69ca
DianaLauraVH <dianalauravhdz@gmail.com>
2021-06-22 21:31:20 -0500

    **
    **

--- gentelella-master/production/MVVM/Login/JsLogin.js
+++ gentelella-master/production/MVVM/Login/JsLogin.js
@@ -1,3 +1,48 @@
 ﻿$(document).ready(function () {
+        localStorage.removeItem('Fecha');
+        //$("#bspinr").hide();
+        jQuery.support.cors = true;
+        $("#bspinr").click(function (e) {
+            //$("#bspinr").show();
 
-});
+            //$("#blogin").removeClass("btn btn-primary btn-block btn-flat");
+
+            e.preventDefault();
+
+            $.post("https://localhost:44376/Token", $("#flogin").serialize(), function (data) {
+
+                localStorage.setItem('tokenKey', JSON.stringify(data));
+
+                window.location.href = "/production/index.html";
+
+            }).fail(function (e) {
+                alert("Usuario / Contraseña Invalido");
+                //$("#bspinr").hide();
+
+            });
+
+
+        });
+
+        var data = JSON.parse(localStorage.getItem('tokenKey'));
+
+
+
+        if (data !== null) {
+
+
+            window.location.href = "/production/index.html";
+        }
+        $("#contra").click(function (e) {
+            e.preventDefault();
+            window.location = configUrl.url("Views/Account/Register.html"); //.pathname = "Views/Account/Register.html";
+        });
+        $("#regi").click(function (e) {
+            window.location = configUrl.url("Views/Account/Register.html");
+
+            e.preventDefault();
+
+        });
+
+
+    });
