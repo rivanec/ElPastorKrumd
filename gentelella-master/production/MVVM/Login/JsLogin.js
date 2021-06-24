01c3df0ee099e59d21f7c667fb6f14bc023fb9eb
DianaLauraVH <dianalauravhdz@gmail.com>
2021-06-23 19:28:02 -0500

    cambios en el index principal comentarios en los js

--- gentelella-master/production/MVVM/Login/JsLogin.js
+++ gentelella-master/production/MVVM/Login/JsLogin.js
@@ -1,6 +1,6 @@
 ﻿
     $(document).ready(function () {
-        localStorage.removeItem('Fecha');
+        localStorage.removeItem('Fecha');  
         //$("#bspinr").hide();
         jQuery.support.cors = true;
         $("#bspinr").click(function (e) {
@@ -8,16 +8,16 @@
 
             //$("#blogin").removeClass("btn btn-primary btn-block btn-flat");
 
-            e.preventDefault();
+            e.preventDefault(); //Cancelar sin detener el resto del funcionamiento.
 
-            $.post("https://localhost:44376/Token", $("#flogin").serialize(), function (data) {
+            $.post("https://localhost:44376/Token", $("#flogin").serialize(), function (data) { //Envio de datos al servidor
 
-                localStorage.setItem('tokenKey', JSON.stringify(data));
+                localStorage.setItem('tokenKey', JSON.stringify(data)); //crea ítems dentro del almacenamiento local
 
-                window.location.href = "/production/index.html";
+                window.location.href = "../../Views/Login/Index.html"; //ubicación del documento 
 
             }).fail(function (e) {
-                alert("Usuario / Contraseña Invalido");
+                alert("Usuario / Contraseña Invalido");  //Error en el usuario y/o cobtraseña
                 //$("#bspinr").hide();
 
             });
@@ -29,21 +29,21 @@
 
 
 
-        if (data !== null) {
+      // if (data !== null) {
 
 
-            window.location.href = "/production/index.html";
-        }
-        $("#contra").click(function (e) {
-            e.preventDefault();
-            window.location = configUrl.url("Views/Account/Register.html"); //.pathname = "Views/Account/Register.html";
-        });
-        $("#regi").click(function (e) {
-            window.location = configUrl.url("Views/Account/Register.html");
+          //  window.location.href = "../../Views/Login/Index.html";
+     //   }
+     //   $("#contra").click(function (e) {
+    //        e.preventDefault();
+    //        window.location = configUrl.url("Views/Account/Register.html"); //.pathname = "Views/Account/Register.html";
+     //   });
+     //   $("#regi").click(function (e) {
+    //        window.location = configUrl.url("Views/Account/Register.html");
 
-            e.preventDefault();
+    //        e.preventDefault();
 
-        });
+    //    });
 
 
     });
